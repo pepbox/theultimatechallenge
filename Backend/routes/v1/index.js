cdcebbf9f09937c4f1809dd75b2839598460f565
Manish <[manishbulchandani4@gmail.com]>
2025-05-20 21:54:51 +0530

    serving build files from server

--- Backend/routes/v1/index.js
+++ Backend/routes/v1/index.js
@@ -3,8 +3,19 @@ const router = express.Router()
 const UltimateChallengeRouter = require("../../modules/theUltimateChallenge/routes/theUltimateChallengeRoutes")
 const SuperAdminRouter = require("../../modules/adminstrators/superAdmin/routes/superAdminRoutes")
 
+
+
 router.use("/theultimatechallenge",UltimateChallengeRouter)
 router.use("/superadmin",SuperAdminRouter)
+
+
+if (process.env.NODE_ENV === "production") {
+  const buildPath = path.join(__dirname, "..", "..", "Frontend", "dist");
+  app.use(express.static(buildPath));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(buildPath, "index.html"));
+  });
+}
 
 
 module.exports = router ;
